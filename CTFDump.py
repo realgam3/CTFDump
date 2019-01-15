07e736e98837746b21583ab5ed79a7ca4de5f4c7
realgam3 <realgam3@gmail.com>
2019-01-15 17:53:35 +0200

    Dirty fix of a bug caused by CTFd 2.0 and category/challenge name sanitization

--- CTFDump.py
+++ CTFDump.py
@@ -15,55 +15,84 @@ session = requests.Session()
 
 
 def download_file(url, file_path):
-    res = session.get(
-        url,
-        verify=False,
-        stream=True,
-    )
-    with open(file_path, 'wb') as f:
-        for chunk in res.iter_content(chunk_size=1024):
-            if not chunk:
-                continue
-            f.write(chunk)
-        f.flush()
+    try:
+        res = session.get(url, stream=True)
+        with open(file_path, 'wb') as f:
+            for chunk in res.iter_content(chunk_size=1024):
+                if not chunk:
+                    continue
+                f.write(chunk)
+            f.flush()
+    except Exception as ex:
+        print ex.message
 
 
-def run(url, username, password):
-    hostname = urlparse(url).hostname
-
+def get_nonce(url):
     res = session.get(
         urljoin(url, "/login"),
         params={'next': 'challenges'}
     )
-    nonce = re.search('<input type="hidden" name="nonce" value="(.*?)">', res.content)
+    return re.search('<input type="hidden" name="nonce" value="(.*?)">', res.content).group(1)
 
-    # Login
+
+def login(url, nonce, username, password):
     session.post(
         urljoin(url, "/login"),
         params={'next': 'challenges'},
         data={
             'name': username,
             'password': password,
-            'nonce': nonce.group(1)
+            'nonce': nonce
         }
     )
 
+
+def logout(url):
     # Get Challenges
-    res = session.get(urljoin(url, "/chals"))
+    session.get(urljoin(url, "/logout"))
+
+
+def iter_challenges(url):
+    # CTFD 2.0
+    res = session.get(urljoin(url, "/api/v1/challenges"))
+    if res.ok:
+        res_json = res.json()
+        challenges = res_json['data']
+        for challenge in challenges:
+            challenge_json = session.get(urljoin(url, "/api/v1/challenges/%d" % challenge['id'])).json()
+            yield challenge_json['data']
+        return
+
+    res_json = session.get(urljoin(url, "/chals")).json()
+    challenges = res_json['game']
+    for challenge in challenges:
+        if 'description' in challenges:
+            yield challenge
+            continue
+
+        yield session.get(urljoin(url, "/chals/%d" % challenge['id'])).json()
+
+
+def run(url, username, password):
+    hostname = urlparse(url).hostname
+
+    # Login
+    login(url, get_nonce(url), username, password)
 
     # Create hostname directory if not exist
     if not path.exists(hostname):
         os.mkdir(hostname)
 
-    res_json = res.json()
-    for challenge in res_json['game']:
-        category_path = path.join(hostname, challenge['category'])
+    # Get Challenges
+    for challenge in iter_challenges(url):
+        category_path = path.join(hostname, re.sub('[^\w\-_ ]', '', challenge['category'].strip()))
         if not path.exists(category_path):
             os.mkdir(category_path)
 
-        challenge_path = path.join(category_path, re.sub('[^\w\-_. ]', '_', challenge['name'].strip()))
+        challenge_path = path.join(category_path, re.sub('[^\w\-_ ]', '', challenge['name'].strip()))
         if not path.exists(challenge_path):
             os.mkdir(challenge_path)
+
             with codecs.open(path.join(challenge_path, "ReadMe.md"), "wb", encoding='utf-8') as f:
                 f.write(u"Name: %s\n" % challenge['name'])
                 f.write(u"Value: %d\n" % challenge['value'])
@@ -79,7 +108,8 @@ def run(url, username, password):
                 fn = path.basename(file_name)
                 download_file(urljoin(url, "/files/%s" % file_name), path.join(challenge_path, fn))
 
-    session.get(urljoin(url, "/logout"))
+    # Logout
+    logout(url)
 
 
 def main(args=None):
