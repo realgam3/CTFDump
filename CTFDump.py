7577cb270fd60e9bd0446bf31228332f883ef356
realgam3 <realgam3@gmail.com>
2020-04-14 20:04:22 +0300

    CTFDump is now extendable to other platforms other then CTFd

--- CTFDump.py
+++ CTFDump.py
@@ -3,116 +3,193 @@ import os
 import sys
 import codecs
 import logging
-import requests
 from os import path
-from requests.sessions import urljoin, urlparse
+from getpass import getpass
+from requests.utils import CaseInsensitiveDict
+from requests.sessions import urljoin, urlparse, Session
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
-__version__ = "0.1.0"
-
-logger = logging.getLogger(__name__)
-session = requests.Session()
+__version__ = "0.2.0"
 
 
-def download_file(url, file_path):
-    try:
-        res = session.get(url, stream=True)
-        with open(file_path, 'wb') as f:
-            for chunk in res.iter_content(chunk_size=1024):
-                if not chunk:
-                    continue
-                f.write(chunk)
-            f.flush()
-    except Exception as ex:
-        print(ex)
+class BadUserNameOrPasswordException(Exception):
+    pass
 
 
-def get_nonce(url):
-    res = session.get(
-        urljoin(url, "/login"),
-        params={'next': 'challenges'}
-    )
-    return re.search('<input type="hidden" name="nonce" value="(.*?)">', res.text).group(1)
+class NotLoggedInException(Exception):
+    pass
 
 
-def login(url, nonce, username, password):
-    session.post(
-        urljoin(url, "/login"),
-        params={'next': 'challenges'},
-        data={
-            'name': username,
-            'password': password,
-            'nonce': nonce
-        }
-    )
+class UnknownFrameworkException(Exception):
+    pass
 
 
-def logout(url):
-    # Get Challenges
-    session.get(urljoin(url, "/logout"))
+class Challenge(object):
+    def __init__(self, session, url, name, category="", description="", files=None, value=0):
+        self.url = url
+        self.name = name
+        self.value = value
+        self.session = session
+        self.category = category
+        self.description = description
+        self.logger = logging.getLogger(__name__)
+        self.files = self.collect_files(files, description)
+
+    @staticmethod
+    def collect_files(files, description=""):
+        files = files or []
+        files.extend(re.findall(r"https?://\w+(?:\.\w+)+(?:/[\w._-]+)+", description, re.DOTALL))
+        return files
+
+    @staticmethod
+    def escape_filename(filename):
+        return re.sub(r"[^\w\s\-.()]", "", filename.strip())
+
+    def download_file(self, url, file_path):
+        try:
+            res = self.session.get(url, stream=True)
+            with open(file_path, 'wb') as f:
+                for chunk in res.iter_content(chunk_size=1024):
+                    if not chunk:
+                        continue
+                    f.write(chunk)
+                f.flush()
+        except Exception as ex:
+            print(ex)
+
+    def dump(self):
+        # Create challenge directory if not exist
+        challenge_path = path.join(
+            self.escape_filename(urlparse(self.url).hostname),
+            self.escape_filename(self.category),
+            self.escape_filename(self.name)
+        )
+        os.makedirs(challenge_path, exist_ok=True)
+
+        with codecs.open(path.join(challenge_path, "ReadMe.md"), "wb", encoding='utf-8') as f:
+            f.write(f"Name: {self.name}\n")
+            f.write(f"Value: {self.value}\n")
+            f.write(f"Description: {self.description}\n")
+            self.logger.info(f"Creating Challenge [{self.category or 'No Category'}] {self.name}")
+
+        for file_url in self.files:
+            file_path = path.join(challenge_path, self.escape_filename(path.basename(urlparse(file_url).path)))
+            self.download_file(file_url, file_path)
 
 
-def iter_challenges(url):
-    # CTFD 2.0
-    res = session.get(urljoin(url, "/api/v1/challenges"))
-    if res.ok:
-        res_json = res.json()
-        challenges = res_json['data']
+class CTF(object):
+    def __init__(self, url):
+        self.url = url
+        self.session = Session()
+        self.logger = logging.getLogger(__name__)
+
+    def iter_challenges(self):
+        raise NotImplementedError()
+
+    def login(self, username, password):
+        raise NotImplementedError()
+
+    def logout(self):
+        raise NotImplementedError()
+
+
+class CTFd(CTF):
+    def __init__(self, url):
+        super().__init__(url)
+
+    @property
+    def version(self):
+        # CTFd >= v2
+        res = self.session.get(urljoin(self.url, "/api/v1/challenges"))
+        if res.status_code == 403:
+            # Unknown (Not logged In)
+            return -1
+
+        if res.status_code != 404:
+            return 2
+
+        # CTFd  >= v1.2
+        res = self.session.get(urljoin(self.url, "/chals"))
+        if res.status_code == 403:
+            # Unknown (Not logged In)
+            return -1
+
+        if 'description' not in res.json()['game'][0]:
+            return 1
+
+        # CTFd  <= v1.1
+        return 0
+
+    def __get_nonce(self):
+        res = self.session.get(urljoin(self.url, "/login"))
+        return re.search('<input type="hidden" name="nonce" value="(.*?)">', res.text).group(1)
+
+    def login(self, username, password):
+        next_url = '/challenges'
+        res = self.session.post(
+            url=urljoin(self.url, "/login"),
+            params={'next': next_url},
+            data={
+                'name': username,
+                'password': password,
+                'nonce': self.__get_nonce()
+            }
+        )
+        if res.ok and urlparse(res.url).path == next_url:
+            return True
+        return False
+
+    def logout(self):
+        self.session.get(urljoin(self.url, "/logout"))
+
+    def __get_file_url(self, file_name):
+        if not file_name.startswith('/files/'):
+            file_name = f"/files/{file_name}"
+        return urljoin(self.url, file_name)
+
+    def __iter_challenges(self):
+        version = self.version
+        if version < 0:
+            raise NotLoggedInException()
+
+        if version >= 2:
+            res_json = self.session.get(urljoin(self.url, "/api/v1/challenges")).json()
+            challenges = res_json['data']
+            for challenge in challenges:
+                challenge_json = self.session.get(urljoin(self.url, f"/api/v1/challenges/{challenge['id']}")).json()
+                yield challenge_json['data']
+
+            return
+
+        res_json = self.session.get(urljoin(self.url, "/chals")).json()
+        challenges = res_json['game']
         for challenge in challenges:
-            challenge_json = session.get(urljoin(url, "/api/v1/challenges/%d" % challenge['id'])).json()
-            yield challenge_json['data']
-        return
+            if version >= 1:
+                yield self.session.get(urljoin(self.url, f"/chals/{challenge['id']}")).json()
+                continue
 
-    res_json = session.get(urljoin(url, "/chals")).json()
-    challenges = res_json['game']
-    for challenge in challenges:
-        if 'description' in challenges:
             yield challenge
-            continue
 
-        yield session.get(urljoin(url, "/chals/%d" % challenge['id'])).json()
+    def iter_challenges(self):
+        for challenge in self.__iter_challenges():
+            yield Challenge(
+                session=self.session, url=self.url,
+                name=challenge['name'], category=challenge['category'],
+                description=challenge['description'],
+                files=list(map(self.__get_file_url, challenge.get('files', [])))
+            )
 
 
-def run(url, username, password):
-    hostname = urlparse(url).hostname
+def get_credentials(username=None, password=None):
+    username = username or os.environ.get('CTF_USERNAME', input('User: '))
+    password = password or os.environ.get('CTF_PASSWORD', getpass('Password: ', stream=False))
 
-    # Login
-    login(url, get_nonce(url), username, password)
+    return username, password
 
-    # Create hostname directory if not exist
-    if not path.exists(hostname):
-        os.mkdir(hostname)
 
-    # Get Challenges
-    for challenge in iter_challenges(url):
-        category_path = path.join(hostname, re.sub('[^\w\-_ ]', '', challenge['category'].strip()))
-        if not path.exists(category_path):
-            os.mkdir(category_path)
-
-        challenge_path = path.join(category_path, re.sub('[^\w\-_ ]', '', challenge['name'].strip()))
-        if not path.exists(challenge_path):
-            os.mkdir(challenge_path)
-
-            with codecs.open(path.join(challenge_path, "ReadMe.md"), "wb", encoding='utf-8') as f:
-                f.write(u"Name: %s\n" % challenge['name'])
-                f.write(u"Value: %d\n" % challenge['value'])
-                f.write(u"Description: %s\n" % challenge['description'])
-                logger.info("Creating Challenge [%s] %s" % (challenge['category'] or "No Category", challenge['name']))
-
-            file_names = re.findall("https?://\w+(?:\.\w+)+(?:/[\w._-]+)+", challenge['description'], re.DOTALL)
-            for file_name in file_names:
-                fn = path.basename(file_name)
-                download_file(file_name, path.join(challenge_path, fn))
-
-            for file_name in challenge['files']:
-                fn = path.basename(urlparse(file_name).path)
-                if not file_name.startswith('/files/'):
-                    file_name = "/files/%s" % file_name
-
-                download_file(urljoin(url, file_name), path.join(challenge_path, fn))
-
-    # Logout
-    logout(url)
+CTFs = CaseInsensitiveDict(data={
+    "CTFd": CTFd
+})
 
 
 def main(args=None):
@@ -124,13 +201,18 @@ def main(args=None):
 
     # Proxy Configuration
     parser.add_argument("url",
-                        help="CTFd URL (For example: https://demo.ctfd.io/)")
+                        help="ctf url (for example: https://demo.ctfd.io/)")
+    parser.add_argument("-c", "--ctf-platform",
+                        choices=CTFs,
+                        help="ctf platform",
+                        default="CTFd")
+    parser.add_argument("-n", "--no-login",
+                        action="store_true",
+                        help="login is not needed")
     parser.add_argument("-u", "--username",
-                        help="CTF username",
-                        required=True)
+                        help="username")
     parser.add_argument("-p", "--password",
-                        help="CTF password.",
-                        required=True)
+                        help="password")
 
     sys_args = vars(parser.parse_args(args=args))
 
@@ -139,8 +221,17 @@ def main(args=None):
                         format='%(asctime)s %(levelname)s %(message)s',
                         datefmt='%d-%m-%y %H:%M:%S')
 
-    # Run CTFDump
-    run(**sys_args)
+    ctf = CTFs.get(sys_args['ctf_platform'])(sys_args['url'])
+    if not sys_args['no_login'] or not os.environ.get('CTF_NO_LOGIN'):
+        print(get_credentials(sys_args['username'], sys_args['password']))
+        if not ctf.login(*get_credentials(sys_args['username'], sys_args['password'])):
+            raise BadUserNameOrPasswordException()
+
+    for challenge in ctf.iter_challenges():
+        challenge.dump()
+
+    if not sys_args['no_login'] or not os.environ.get('CTF_NO_LOGIN'):
+        ctf.logout()
 
 
 if __name__ == '__main__':
